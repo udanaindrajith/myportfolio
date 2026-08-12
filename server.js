36ae3970d761d197184c16bcb7bf761ff31ce174
Udana Indrajith <126150846+udanaindrajith@users.noreply.github.com>
2026-08-12 15:41:15 +0400

    Change default file served from in.html to index.html

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const server = http.createServer((req, res) => {
   
   let filePath = '.' + req.url;
   if (filePath === './') {
-    filePath = './in.html';
+    filePath = './index.html';
   }
   
   const extname = String(path.extname(filePath)).toLowerCase();
